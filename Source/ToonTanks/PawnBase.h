9934deb89e118514ae6361d1c7257cf9717d0fc1
No Nickle <zzhao1573@gmail.com>
2020-07-14 17:59:52 -0700

    c++ code

---
+++ Source/ToonTanks/PawnBase.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PawnBase.generated.h"
+
+
+class UCapsuleComponent;
+
+UCLASS()
+class TOONTANKS_API APawnBase : public APawn
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY()
+	UCapsuleComponent* CapsuleComp;
+	UPROPERTY()
+	UStaticMeshComponent* BaseMesh;
+	UPROPERTY()
+	UStaticMeshComponent* TurrentMesh;
+	UPROPERTY()
+	USceneComponent* ProjectileSpawnPoint;
+
+
+public:
+	// Sets default values for this pawn's properties
+	APawnBase();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+};
